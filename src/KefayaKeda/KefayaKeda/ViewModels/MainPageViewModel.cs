54c83ea6c9b2bee90f34dd4d5c553087b9622bc9
Abdelrahman El Kinani <kinani95@outlook.com>
2016-08-24 03:18:27 +0200

    finalize MVVM structure

---
+++ src/KefayaKeda/KefayaKeda/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KefayaKeda.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+        public MainPageViewModel() { }
+    }
+}
