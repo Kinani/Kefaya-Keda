4ec409382e7634a1507713dcab890db79ee94beb
Abdelrahman El Kinani <kinani95@outlook.com>
2016-08-28 12:35:42 +0200

    Major commit[working BK service]

--- src/KefayaKeda/KefayaKeda/ViewModels/MainPageViewModel.cs
+++ src/KefayaKeda/KefayaKeda/ViewModels/MainPageViewModel.cs
@@ -11,7 +11,10 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
 using Windows.Storage;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 
 namespace KefayaKeda.ViewModels
@@ -26,6 +29,9 @@ namespace KefayaKeda.ViewModels
 
         #region Properties
         private StorageFile capturedImage { get; set; }
+        private AppServiceConnection appServiceConnection { get; set; }
+        public bool isWorking { get; set; } = true;
+
         #endregion
 
         #region Commands
@@ -36,6 +42,10 @@ namespace KefayaKeda.ViewModels
             {
                 captureEleLoaded = new RelayCommand(async () =>
                 {
+                    if (appServiceConnection == null)
+                    {
+                        await InitAppSvc();
+                    }
                     _cameraMan = new CameraMan(captureElement);
                     await _cameraMan.StartPreviewAsync();
                     StartWork();
@@ -57,6 +67,72 @@ namespace KefayaKeda.ViewModels
             VisualFeature[] visualFeatures = new VisualFeature[] {VisualFeature.Description, VisualFeature.Tags };
             var fileStream = await file.OpenAsync(FileAccessMode.Read);
             return await visonClient.AnalyzeImageAsync(fileStream.AsStream(), visualFeatures);
+        }
+        private async Task InitAppSvc()
+        {
+            // Initialize the AppServiceConnection
+            appServiceConnection = new AppServiceConnection();
+            appServiceConnection.PackageFamilyName = "3794d190-02a2-4c83-b044-74c5304739e9_7fvddcazhy2xa";
+            //appServiceConnection.AppServiceName = "BackgroundWebServer-uwp";
+            appServiceConnection.AppServiceName = "MySuperServer";
+            // Send a initialize request 
+            var res = await appServiceConnection.OpenAsync();
+            if (res == AppServiceConnectionStatus.Success)
+            {
+                var message = new ValueSet();
+                message.Add("Command", "Initialize");
+                var response = await appServiceConnection.SendMessageAsync(message);
+                if (response.Status != AppServiceResponseStatus.Success)
+                {
+                    throw new Exception("Failed to send message");
+                }
+                appServiceConnection.RequestReceived += OnMessageReceived;
+            }
+        }
+
+        private async void OnMessageReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            var message = args.Request.Message;
+            string newState = message["State"] as string;
+            switch (newState)
+            {
+                case "On":
+                    {
+                        await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher
+                       .RunAsync(
+                              CoreDispatcherPriority.High,
+                             () =>
+                             {
+                                 if(!isWorking)
+                                 {
+                                     StartWork();
+                                     isWorking = true;
+                                 }
+                             });
+                        break;
+                    }
+                case "Off":
+                    {
+                        await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher
+                       .RunAsync(
+                              CoreDispatcherPriority.High,
+                             () =>
+                             {
+                                 if(isWorking)
+                                 {
+                                     StopWork();
+                                     isWorking = false;
+                                 }
+                             });
+                        break;
+                    }
+                case "Unspecified":
+                default:
+                    {
+                        // Do nothing 
+                        break;
+                    }
+            }
         }
 
         void StopWork()
